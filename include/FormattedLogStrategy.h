f8092154a35a8ee838b94c97932f433d170c381a
sebitse <sebastian.tonu14@gmail.com>
2024-09-02 23:30:45 +0300

    Formatted strategy done

--- include/FormattedLogStrategy.h
+++ include/FormattedLogStrategy.h
@@ -1,11 +1,21 @@
 #ifndef FORMATTEDLOG_H
 #define FORMATTEDLOG_H
 
+#include <sstream>
+#include <iomanip>
+
 #include "LogStrategy.h"
 
 class FormattedLogStrategy : public LogStrategy
 {
+    private:
+        std::string format;
+        void replacePlaceholders(std::string& str, const std::string& placeholder, const std::string& value);
+        std::string formatMessage(const std::string& message);
+        std::string currentTime();
     public:
+        FormattedLogStrategy(const std::string& format = "[%H:%M:%S] {MESSAGE}");
+        void setFormat(const std::string&);
         void log(std::string key, std::ofstream& logfile) override;
 };
 
--- src/FormattedLogStrategy.cpp
+++ src/FormattedLogStrategy.cpp
@@ -1,6 +1,46 @@
 #include "../include/FormattedLogStrategy.h"
 
+ FormattedLogStrategy::FormattedLogStrategy(const std::string& _format = "[%H:%M:%S] {MESSAGE}") :
+                format(_format) {}
+
+void FormattedLogStrategy::setFormat(const std::string& _format) 
+{
+    this->format = _format;    
+}
+
+void FormattedLogStrategy::replacePlaceholders(std::string& str, const std::string& placeholder, const std::string& value)
+{
+    size_t pos = str.find(placeholder);
+
+    if (pos != std::string::npos) 
+    {
+        str.replace(pos, placeholder.length(), value);
+    }
+}
+std::string FormattedLogStrategy::formatMessage(const std::string& message)
+{
+    std::string aux = this->format;
+
+    replacePlaceholders(aux, "{MESSAGE}", message);
+    replacePlaceholders(aux, "%H:%M:%S", currentTime());
+    
+    return aux;
+}
+
+std::string FormattedLogStrategy::currentTime()
+{
+    std::ostringstream oss;
+    auto t = std::time(nullptr);
+    auto tm = *std::localtime(&t);
+    oss << std::put_time(&tm, "%H:%M:%S");
+
+    return oss.str();
+}
+
 void FormattedLogStrategy::log(std::string key, std::ofstream& logfile) 
 {
-    
+    std::string formattedMessage = formatMessage(key);
+
+    logfile << formattedMessage;
+    logfile.flush();
 }
